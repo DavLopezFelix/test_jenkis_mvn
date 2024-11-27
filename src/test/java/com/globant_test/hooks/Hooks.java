cb9f637642953ff950bc837c1a823e158edbf2b4
David Lop <dlopezf.19@gmail.com>
2024-11-26 19:16:19 -0500

    take screenshot and pdf report

--- src/test/java/com/globant_test/hooks/Hooks.java
+++ src/test/java/com/globant_test/hooks/Hooks.java
@@ -3,6 +3,9 @@ package com.globant_test.hooks;
 import com.globant_test.config.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -14,9 +17,16 @@ public class Hooks {
     }
 
     @After
+    public static void takeScreenshot(Scenario scenario){
+        if (scenario.isFailed()){
+            TakesScreenshot takesScreenShot = (TakesScreenshot) DriverFactory.getDriver();
+            byte [] screenshot = takesScreenShot.getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", screenshot.toString());
+        }
+    }
+
     public void tearDown(){
         DriverFactory.quitDriver();
         System.out.println("Aplicacion cerrada!");
-
     }
 }
